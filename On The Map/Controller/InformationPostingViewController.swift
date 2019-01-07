8c8ed0037760d177d985b07dd5bea4802afa6769
rosarioarobinson <rosario.a.robinson@gmail.com>
2019-01-07 18:29:27 -0500

    added view controllers for information posting view

---
+++ On The Map/Controller/InformationPostingViewController.swift
@@ -0,0 +1,9 @@
+//
+//  InformationPostingViewController.swift
+//  On The Map
+//
+//  Created by Rosario A Robinson on 1/7/19.
+//  Copyright © 2019 Rosario Robinson. All rights reserved.
+//
+
+import Foundation
