9eb074f718295ebd4a457d60a664a0db9f2105fa
rosarioarobinson <rosario.a.robinson@gmail.com>
2019-01-08 21:31:04 -0500

    updates to parse convenience file

--- On The Map/Model/Parse/ParseConvenience.swift
+++ On The Map/Model/Parse/ParseConvenience.swift
@@ -13,26 +13,26 @@ extension ParseClient {
     
     //MARK: Authenticating Users
     
-    private func getStudentLocations(_ requestToken: String?, completionHandlerForSession: @escaping (_ success: Bool, _ sessionID: String?, _ errorString: String?) -> Void) {
+    private func getStudentLocations<ResponseType: Decodable>(url: URL, response: ResponseType.Type, completionHandlerForGET: @escaping (ResponseType?, Error?) -> Void) {
         
         /* 1. Specify parameters, method (if has {key}), and HTTP body (if POST) */
-        let parameters = [ParseConstants.JSONResponseKeys.requestToken: requestToken!]
+        let parameters = [ParseConstants.JSONResponseKeys.requestToken]
         
         /* 2. Make the request */
-        let _ = taskForGetMultipleLocations(url:(url: URL(string: "https://parse.udacity.com/parse/classes/StudentLocation")!) as! URL, response: parameters as [AnyObject]) { (results, error) in
+        let _ = taskForGetMultipleLocations(url: URL(string: "https://parse.udacity.com/parse/classes/StudentLocation")!, response: String.self) { (results, error) in
             
         var request = URLRequest(url: URL(string: "https://parse.udacity.com/parse/classes/StudentLocation")!)
             
             /* 3. Send the desired value(s) to completion handler */
             if let error = error {
                 print(error)
-                completionHandlerForGetALocation(false, nil, "Login Failed (Session ID).")
+                completionHandlerForGET(false, nil, "Login Failed (Session ID).")
             } else {
                 if let sessionID = results?[ParseConstants.JSONResponseKeys.SessionID] as? String {
-                    completionHandlerForSession(true, sessionID, nil)
+                    completionHandlerForGET(true, sessionID, nil)
                 } else {
                     print("Could not find \(ParseConstants.JSONResponseKeys.SessionID) in \(results)")
-                    completionHandlerForSession(false, nil, "Login Failed (Session ID).")
+                    completionHandlerForGET(false, nil, "Login Failed (Session ID).")
                 }
             }
         }
