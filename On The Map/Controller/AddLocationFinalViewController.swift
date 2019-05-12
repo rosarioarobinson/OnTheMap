9cacb2e23fdc5e485eee568556d847439bac1340
rosarioarobinson <rosario.a.robinson@gmail.com>
2019-05-11 23:34:31 -0400

    con't changes

--- On The Map/Controller/AddLocationFinalViewController.swift
+++ On The Map/Controller/AddLocationFinalViewController.swift
@@ -19,44 +19,53 @@ class AddLocationFinalViewController: UIViewController, MKMapViewDelegate {
     @IBOutlet weak var finishButton: UIButton!
     
     
+    var annotations = [MKPointAnnotation]()
+    
     
     //lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        mapView.delegate = self
-            
-            //insert StudentLocation structs
-            for student in StudentArray.sharedInstance.studentsArray {
-                
-                //latitude and longitude
-                let lat = CLLocationDegrees(student.latitude ?? 0)
-                let long = CLLocationDegrees(student.longitude ?? 0)
-                
-                //latitude and longitude are used to create CLLocationCoordinates2D instance.
-                let coordinate = CLLocationCoordinate2D(latitude: lat, longitude: long)
-                
-                let first = student.firstName ?? ""
-                let last = student.lastName ?? ""
-                let mediaURL = student.mediaURL
-                
-                //Here we create the annotation and set its coordiate, title, and subtitle properties
-                let annotation = MKPointAnnotation()
-                annotation.coordinate = coordinate
-                annotation.title = "\(String(describing: first)) \(String(describing: last))"
-                annotation.subtitle = mediaURL
-                
-                //Finally we place the annotation in an array of annotations.
-                self.annotations.append(annotation)
         
-    }
+        mapView.delegate = self
+        
+        //insert StudentLocation structs
+        for student in StudentArray.sharedInstance.studentsArray {
+            
+            //latitude and longitude
+            let lat = CLLocationDegrees(student.latitude ?? 0)
+            let long = CLLocationDegrees(student.longitude ?? 0)
+            
+            //latitude and longitude are used to create CLLocationCoordinates2D instance.
+            let coordinate = CLLocationCoordinate2D(latitude: lat, longitude: long)
+            
+            // represent latitude of the new location
+            let region = MKCoordinateRegion(center: coordinate, latitudinalMeters: 0.25, longitudinalMeters: 0.25);
+            
+            mapView.setRegion(region, animated: true)
+            
+            let first = student.firstName ?? ""
+            let last = student.lastName ?? ""
+            let mediaURL = student.mediaURL
+            
+            //Here we create the annotation and set its coordiate, title, and subtitle properties
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = coordinate
+            annotation.title = "\(String(describing: first)) \(String(describing: last))"
+            annotation.subtitle = mediaURL
+            
+            //Finally we place the annotation in an array of annotations.
+            self.annotations.append(annotation)
+            
+        }
         
         //When the array is complete, we add the annotations to the map.
         DispatchQueue.main.async {
             self.mapView.addAnnotations(self.annotations)
         }
+    }
     
-        func viewWillAppear(_ animated: Bool) {
+    override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
     }
     
@@ -67,39 +76,35 @@ class AddLocationFinalViewController: UIViewController, MKMapViewDelegate {
         
     }
     
-        //MARK: MKMapViewDelegate
+    //MARK: MKMapViewDelegate
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
         
-        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-            
-            let reuseId = "pin"
-            
-            var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
-            
-            if pinView == nil {
-                pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
-                pinView!.canShowCallout = true
-                pinView!.pinTintColor = .red
-                pinView!.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
-            }
-            else {
-                pinView!.annotation = annotation
-            }
-            
-            return pinView
+        let reuseId = "pin"
+        
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
+        
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.canShowCallout = true
+            pinView!.pinTintColor = .red
+            pinView!.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        }
+        else {
+            pinView!.annotation = annotation
         }
         
-        //Method implemented to respond to taps
-        func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
-            if control == view.rightCalloutAccessoryView {
-                let app = UIApplication.shared
-                if let toOpen = view.annotation?.subtitle! {
-                    app.openURL(URL(string: toOpen)!)
-                }
+        return pinView
+    }
+    
+    //Method implemented to respond to taps
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        if control == view.rightCalloutAccessoryView {
+            let app = UIApplication.shared
+            if let toOpen = view.annotation?.subtitle! {
+                app.openURL(URL(string: toOpen)!)
             }
         }
-        
-        
-        
     }
 
 
